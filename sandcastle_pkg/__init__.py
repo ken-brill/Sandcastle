a8eb0290f529800339a867ac5ed3f0617b536824
Ken Brill <ken.brill@gmail.com>
2025-12-25 13:01:10 -0600

    Fix import name: prefetch_picklists_for_object - v1.0.6

--- sandcastle_pkg/__init__.py
+++ sandcastle_pkg/__init__.py
@@ -5,5 +5,5 @@ A two-phase data migration tool that copies production Salesforce data
 into development sandboxes while preserving all relationships.
 """
 
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 __author__ = "Ken Brill"
--- sandcastle_pkg/utils/__init__.py
+++ sandcastle_pkg/utils/__init__.py
@@ -8,7 +8,7 @@ from .record_utils import (
 )
 from .csv_utils import write_record_to_csv, read_migration_csv, clear_migration_csvs
 from .bulk_utils import BulkRecordCreator
-from .picklist_utils import get_valid_picklist_values, prefetch_picklist_values
+from .picklist_utils import get_valid_picklist_values, prefetch_picklists_for_object
 
 __all__ = [
     'check_record_exists',
@@ -20,5 +20,5 @@ __all__ = [
     'clear_migration_csvs',
     'BulkRecordCreator',
     'get_valid_picklist_values',
-    'prefetch_picklist_values'
+    'prefetch_picklists_for_object'
 ]
